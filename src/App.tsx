399c6bc2f0a38bece817a34468397e6775333a2f
ivandax <ivandax_89@hotmail.com>
2026-01-14 19:48:19 +0100

    Split into right panel and left panel

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ function App() {
   const basket = useBasket(catalogue, deliveryRules);
 
   return (
-    <div>
-      <ProductCatalog catalogue={catalogue} onProductClick={basket.add} />
+    <div className="app-container">
+      <div className="left-panel">
+        <ProductCatalog catalogue={catalogue} onProductClick={basket.add} />
+      </div>
+      <div className="right-panel">
+        {/* Basket will go here */}
+      </div>
     </div>
   );
 }
